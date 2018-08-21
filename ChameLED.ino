b290372acc77898e4d1c6bb80d4286e9c54a7b93
Ashley <ashley.qian@duke.edu>
2018-08-21 08:45:42 -0700

    Switched out Neopixel lib to use FastLED.

--- ChameLED.ino
+++ ChameLED.ino
@@ -1,10 +1,16 @@
+
+#include "FastLED.h"
 #include <Wire.h>
-#include <Adafruit_NeoPixel.h>
 #include "Adafruit_TCS34725.h"
-#define PIN 8
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
 
+#define DATA_PIN   6
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define NUM_LEDS    60       // Change this to reflect the number of LEDs you have
+#define BRIGHTNESS  255      // Set brightness here
+
+CRGB leds[NUM_LEDS];
 
 /* Example code for the Adafruit TCS34725 breakout library */
 
@@ -22,7 +28,6 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS347
 int greenPin = 6;
 int bluePin = 9;
 int redPin = 10;
-int led = 7;
 int thumbButton = 12;
 
 // our RGB -> eye-recognized gamma color
@@ -31,12 +36,14 @@ byte gammatable[256];
 
 void setup(void) {
   Serial.begin(9600);
-  
-  pinMode(greenPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  pinMode(led, OUTPUT);
-  pinMode(thumbButton, INPUT_PULLUP);
+  delay(3000);
+
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+    .setCorrection(TypicalLEDStrip) // cpt-city palettes have different color balance
+    .setDither(BRIGHTNESS < 255);
+
+  FastLED.setBrightness(BRIGHTNESS);
 
   // this helps convert RGB colors to what humans see
   for (int i=0; i<256; i++) {
@@ -46,87 +53,55 @@ void setup(void) {
     x *= 255;
     gammatable[i] = x;      
   }
-
-  strip.begin();
-  strip.setBrightness(255);
-  strip.show();
 }
-
-boolean isSensorLEDOff = true;
 
 void loop(void) {
   uint16_t r, g, b, c, colorTemp, lux;
-  
-  tcs.setInterrupt(true);
-  int thumbInput = digitalRead(thumbButton);
 
-  Serial.println(thumbInput);
+  tcs.getRawData(&r, &g, &b, &c);
+  colorTemp = tcs.calculateColorTemperature(r, g, b);
+  lux = tcs.calculateLux(r, g, b);
+  
+  uint32_t sum = r;
+  sum += g;
+  sum += b;
 
-  if (thumbInput == 0) {
-    tcs.setInterrupt(false);
+  float red, green, blue;
+
+  red = r; red /= sum;
+  green = g; green /= sum;
+  blue = b; blue /= sum;
+  red *= 256; green *= 256; blue *= 256;
+
+  // Apparently the gammatable version results in better color
+  int humanRed = (int)gammatable[(int)red];
+  int humanGreen = (int)gammatable[(int)green];
+  int humanBlue = (int)gammatable[(int)blue];
     
-    tcs.getRawData(&r, &g, &b, &c);
-    colorTemp = tcs.calculateColorTemperature(r, g, b);
-    lux = tcs.calculateLux(r, g, b);
+  Serial.print((int)red, HEX);
+  Serial.print((int)green, HEX);
+  Serial.print((int)blue, HEX);
   
-    uint32_t sum = r;
-    sum += g;
-    sum += b;
+  Serial.println(" ");
   
-    float red, green, blue;
+  Serial.print(humanRed);
+  Serial.print(" ");
+  Serial.print(humanGreen);
+  Serial.print(" ");
+  Serial.print(humanBlue);
   
-    red = r; red /= sum;
-    green = g; green /= sum;
-    blue = b; blue /= sum;
-    red *= 256; green *= 256; blue *= 256;
+  Serial.println(" ");
   
-    // Apparently the gammatable version results in better color
-    float humanRed = gammatable[(int)red];
-    float humanGreen = gammatable[(int)green];
-    float humanBlue = gammatable[(int)blue];
+  Serial.print(red);
+  Serial.print(" ");
+  Serial.print(green);
+  Serial.print(" ");
+  Serial.print(blue);
   
-    
-    // For anode LEDs 255 is no brightness and 0 is max brightness
-    float anodeLEDRed = 255 - red;
-    float anodeLEDGreen = 255 - green;
-    float anodeLEDBlue = 255 - blue;
-    
-    Serial.print((int)red, HEX);
-    Serial.print((int)green, HEX);
-    Serial.print((int)blue, HEX);
-    
-    Serial.println(" ");
-    
-    Serial.print(humanRed);
-    Serial.print(" ");
-    Serial.print(humanGreen);
-    Serial.print(" ");
-    Serial.print(humanBlue);
-    
-    Serial.println(" ");
-    
-    Serial.print(red);
-    Serial.print(" ");
-    Serial.print(green);
-    Serial.print(" ");
-    Serial.print(blue);
-    
-    Serial.println(" ");
-    
-    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.print(" K - ");
-    Serial.print("Lux: "); Serial.print(lux, DEC); Serial.print(" - ");
-    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
-    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
-    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
-    Serial.print("C: "); Serial.print(c, DEC); Serial.print(" ");
-    Serial.println(" ");
-    Serial.println(" ");
-  
-    analogWrite(greenPin, anodeLEDGreen);
-    analogWrite(redPin, anodeLEDRed);
-    analogWrite(bluePin, anodeLEDBlue);
-  
-    strip.setPixelColor (0, strip.Color(humanRed, humanGreen, humanBlue));
-    strip.show();
+  Serial.println(" ");
+
+  for(int i = 0 ; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(humanRed, humanGreen, humanBlue);
   }
+  FastLED.show();
 }
