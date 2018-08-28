c6adc734d77bb87a599ce4c95bce54036699ea6f
Ashley <ashley.qian@duke.edu>
2018-08-27 19:54:14 -0700

    Bring back thumb input based color sensing.

--- ChameLED.ino
+++ ChameLED.ino
@@ -2,13 +2,21 @@
 #include "FastLED.h"
 #include <Wire.h>
 #include "Adafruit_TCS34725.h"
+#include "Adafruit_Pixie.h"
+#include "SoftwareSerial.h"
 
-
-#define DATA_PIN   6
+#define SELECTOR_PIN 10
+#define LED_PIN   6
+#define PIXIE_PIN  9
+#define NUM_PIXELS 3
 #define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
 #define NUM_LEDS    60       // Change this to reflect the number of LEDs you have
 #define BRIGHTNESS  255      // Set brightness here
+#define UPDATES_PER_SECOND 100
+
+SoftwareSerial pixieSerial(-1, PIXIE_PIN);
+Adafruit_Pixie strip = Adafruit_Pixie(NUM_PIXELS, &pixieSerial);
 
 CRGB leds[NUM_LEDS];
 
@@ -25,25 +33,25 @@ CRGB leds[NUM_LEDS];
 /* Initialise with specific int time and gain values */
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
 
-int greenPin = 6;
-int bluePin = 9;
-int redPin = 10;
-int thumbButton = 12;
 
 // our RGB -> eye-recognized gamma color
 byte gammatable[256];
 
-
 void setup(void) {
   Serial.begin(9600);
-  delay(3000);
-
+  
   // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
     .setCorrection(TypicalLEDStrip) // cpt-city palettes have different color balance
     .setDither(BRIGHTNESS < 255);
-
   FastLED.setBrightness(BRIGHTNESS);
+  
+  pixieSerial.begin(115200);
+  strip.setBrightness(BRIGHTNESS);  // Adjust as necessary to avoid blinding
+  
+  pinMode(SELECTOR_PIN, INPUT_PULLUP);
+  
+  tcs.setInterrupt(true);
 
   // this helps convert RGB colors to what humans see
   for (int i=0; i<256; i++) {
@@ -55,53 +63,61 @@ void setup(void) {
   }
 }
 
+uint16_t humanRed;
+uint16_t humanGreen;
+uint16_t humanBlue;
+boolean isSensorLEDOff = true;
+
 void loop(void) {
   uint16_t r, g, b, c, colorTemp, lux;
-
-  tcs.getRawData(&r, &g, &b, &c);
-  colorTemp = tcs.calculateColorTemperature(r, g, b);
-  lux = tcs.calculateLux(r, g, b);
   
-  uint32_t sum = r;
-  sum += g;
-  sum += b;
+  tcs.setInterrupt(true);
 
-  float red, green, blue;
+  int thumbInput = digitalRead(SELECTOR_PIN);
+  if (thumbInput == 0) {
+    Serial.println("true");
+    tcs.setInterrupt(false);
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature(r, g, b);
+    lux = tcs.calculateLux(r, g, b);
 
-  red = r; red /= sum;
-  green = g; green /= sum;
-  blue = b; blue /= sum;
-  red *= 256; green *= 256; blue *= 256;
-
+    uint32_t sum = r;
+    sum += g;
+    sum += b;
+  
+    float red, green, blue;
+  
+    red = r; red /= sum;
+    green = g; green /= sum;
+    blue = b; blue /= sum;
+    red *= 256; green *= 256; blue *= 256;
+  
   // Apparently the gammatable version results in better color
-  int humanRed = (int)gammatable[(int)red];
-  int humanGreen = (int)gammatable[(int)green];
-  int humanBlue = (int)gammatable[(int)blue];
-    
-  Serial.print((int)red, HEX);
-  Serial.print((int)green, HEX);
-  Serial.print((int)blue, HEX);
+    humanRed = (uint16_t)gammatable[(int)red];
+    humanGreen = (uint16_t)gammatable[(int)green];
+    humanBlue = (uint16_t)gammatable[(int)blue];
   
-  Serial.println(" ");
+    int pixelRed = (int)gammatable[(int)red];
+    int pixelGreen = (int)gammatable[(int)green];
+    int pixelBlue = (int)gammatable[(int)blue];
   
-  Serial.print(humanRed);
-  Serial.print(" ");
-  Serial.print(humanGreen);
-  Serial.print(" ");
-  Serial.print(humanBlue);
+  //  Gammatable works better because it gives a higher
+  //  variety of color
+  //  humanRed = (int)red;
+  //  humanGreen = (int)green;
+  //  humanBlue = (int)blue;
   
-  Serial.println(" ");
-  
-  Serial.print(red);
-  Serial.print(" ");
-  Serial.print(green);
-  Serial.print(" ");
-  Serial.print(blue);
-  
-  Serial.println(" ");
-
-  for(int i = 0 ; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(humanRed, humanGreen, humanBlue);
+  //  Serial.println(pixelRed);
+  //  Serial.println(pixelGreen);
+  //  Serial.println(pixelBlue);
+  //
+  //  strip.setPixelColor(0, 255, 255, 255);
+  //  strip.show();
+  //  delay(10);
+  //  
+    for(uint8_t i = 0 ; i < NUM_LEDS; i++) {
+      leds[i] = CRGB(humanRed, humanGreen, humanBlue);
+    }
+    FastLED.show();
   }
-  FastLED.show();
 }
